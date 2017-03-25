f6c044b57816205715fc290d25d09708dd0dc0d8
joyce chan <jchan@change.org>
2017-03-24 18:43:00 -0700

    init commit, based on coreos-vagrant public repo

---
+++ provision/shell.sh
@@ -0,0 +1,5 @@
+
+mkdir /opt
+mkdir /opt/bin
+curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod 755 /usr/local/bin/docker-compose
